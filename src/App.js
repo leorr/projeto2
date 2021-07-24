367e08b58cfbb6b93772423586165a6e2c3ae1a4
Leonardo Rodrigues Ribeiro <leorr112624@gmail.com>
2021-07-24 18:34:46 -0300

    consumindo dados da api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
+
   state = {
     weather: []
   }
-  componentDidMount() {
-    fetch('http://ladataverte.fr/api/1.0/data_points?id_indicators[]=10&type_place[]=world&from=2010b-01-01&to=2010-12-31')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ weather: data })
+
+  async getData() {
+    await axios.get(`http://ladataverte.fr/api/1.0/data_points?id_indicators[]=10&type_place[]=world&from=2010-01-01&to=2010-12-31`)
+    .then( res => {
+      const weather = res.data;
+      //set state format data weather csv -> json
+      this.setState({ weather });
     })
   }
+
+  componentDidMount() {
+    this.getData();
+    //plot data
+  }
+
   render () {
     return (
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Projeto 2</h5>
-          <h6 class="card-subtitle mb-2 text-muted">teste deploy</h6>
-          <p class="card-text">Commit inicial</p>
+          <h5 class="card-title">Projeto 2, segue abaixo dados do clima do mundo no mundo em 2010 sem formatação.</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{this.state.weather}</h6>
         </div>
       </div>
-      );
-    }
+    );
   }
+}
+
 export default App;
