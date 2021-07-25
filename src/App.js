d56cd0ae8ae5357f5799309943c98bb6d4e6b70c
Leonardo Rodrigues Ribeiro <leorr112624@gmail.com>
2021-07-25 04:53:21 -0300

    plots sobrepostos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 import { csvJSON } from './utils/CsvUtils'
 
@@ -8,7 +8,8 @@ class App extends Component {
 
   state = {
     weathers: [],
-    years: [2005, 2006, 2007, 2008, 2009, 2010, 2011],
+    sumTemp: [],
+    years: [2000, 2005, 2010],
     loading: true,
     error: false,
   };
@@ -25,8 +26,9 @@ class App extends Component {
           const weather = csvJSON(csvString)
           .map(value => value[2])
           .filter(function (element) { return element !== undefined; })
-          .flatMap(value => ({ [year]: parseFloat(value) }));
-          weathers.push(weather);
+          .flatMap(value => parseFloat(value));
+          this.state.sumTemp.push(weather.reduce((a, b) => a + b, 0))
+          weathers.push(weather.flatMap(value => ({ [year]: value })));
         });
       } catch (error) {
         this.setState({ error: true });
@@ -42,68 +44,57 @@ class App extends Component {
   
   componentDidMount() {
     this.getData(this.state.years);
-    //plot data
   }
 
   render() {
     if(!this.state.loading) {
-      const data = [].concat.apply([],this.state.weathers);
-      console.log(data);
+      const data = this.state.weathers;
+      var arr = [];
+      for (let index = 0; index < 12; index++) {
+        const test = [
+          {'2000':data[0][index][2000],'2005':data[1][index][2005],'2010':data[2][index][2010]}
+        ];
+        arr.push(test);
+      }
+      var finalarr = [].concat.apply([], arr)
       return (
         <div>
+          Dados da média de temperatura global (Celsius) por ano, recuperados da api <a href="http://ladataverte.fr">ladataverte</a>
           <LineChart
           width={600}
-          height={400}
-          data={data}
+          height={600}
+          data={finalarr}
           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          tittle=""
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
           <Line
           type='monotone'
+          dataKey='2000'
+          stroke='#8884d8'
+          fill='#8884d8'
+          name='2000'
+          />
+          <Line
+          type='monotone'
           dataKey='2005'
-          stroke='#8884d8'
-          fill='#8884d8'
-          />
-          <Line
-          type='monotone'
-          dataKey='2006'
-          stroke='#8884d8'
-          fill='#8884d8'
-          />
-          <Line
-          type='monotone'
-          dataKey='2007'
-          stroke='#8884d8'
-          fill='#8884d8'
-          />
-          <Line
-          type='monotone'
-          dataKey='2008'
-          stroke='#8884d8'
-          fill='#8884d8'
-          />
-          <Line
-          type='monotone'
-          dataKey='2009'
-          stroke='#8884d8'
-          fill='#8884d8'
+          stroke='#fff000'
+          fill='#fff000'
           />
           <Line
           type='monotone'
           dataKey='2010'
-          stroke='#8884d8'
-          fill='#8884d8'
-          />
-          <Line
-          type='monotone'
-          dataKey='2011'
-          stroke='#8884d8'
-          fill='#8884d8'
+          stroke='#ff0000'
+          fill='#ff0000'
           />
           <Tooltip />
         </LineChart>
+        A soma das médias de temperatura por ano<br/> <br/>
+        2000: {this.state.sumTemp[0]} <br/>
+        2005: {this.state.sumTemp[1]} <br/>
+        2010: {this.state.sumTemp[2]} <br/>
         </div>
       );
     } else {
