86eaded5fa835d9c0e373ea280853926ffeec248
leorr <leorr112624@gmail.com>
2021-08-29 23:26:16 -0300

    2021/08/29 23:26 add signUp interface

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,6 +8,10 @@ async function loginUser(credentials) {
    return axios.post(`https://projeto3-leorr.herokuapp.com/user/login`, credentials)
 }
 
+async function registerUser(credentials) {
+   return axios.post(`https://projeto3-leorr.herokuapp.com/user/signup`, credentials)
+}
+
 export default function Login({ setToken }) {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
@@ -19,10 +23,32 @@ export default function Login({ setToken }) {
     });
     setToken(res.data);
   }
+  const handleRegister = async e => {
+    e.preventDefault();
+    const res = await registerUser({
+      username,
+      password
+    });
+    setToken(res.data);
+  }
   return(
     <div className="login-wrapper">
       <h1>Informações de login</h1>
       <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={e => setUserName(e.target.value)}/>
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <h1>Informações de registro</h1>
+      <form onSubmit={handleRegister}>
         <label>
           <p>Username</p>
           <input type="text" onChange={e => setUserName(e.target.value)}/>
